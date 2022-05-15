6c16444e15112278955002ebc3e64aaac190dacd
Jonathan Gaegler <jon@kindful.com>
2022-05-15 03:00:46 -0400

    Get ruby version upgrade working

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,17 @@
 class Player < ApplicationRecord
   has_many :player_cards
+  has_many :scars
+  has many :upgrades
 
   def draw
     card = PlayerCard.draw
     card.update(player: self)
+  end
+
+  # Traveling salesman. Will cache location since they are finite
+  def available_locations
+  end
+
+  def move
   end
 end
--- db/migrate/20220514190241_create_players.rb
+++ db/migrate/20220514190241_create_players.rb
@@ -1,7 +1,8 @@
 class CreatePlayers < ActiveRecord::Migration[7.0]
   def change
     create_table :players do |t|
-
+      t.belongs_to :city
+      t.boolean :dead, default: false
       t.timestamps
     end
   end
